2bc152789adf4de3eae5b8385e1afc414432fffa
Rifki Ardiansyah <rifkiardiansyah670@gmail.com>
2023-02-22 15:34:38 +0700

    ReLU

---
+++ p5.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+np.random.seed(0)
+
+# X is standard for input denotation.
+X = [
+    [1, 2, 3, 2.5],
+    [2.0, 5.0, -1.0, 2.0],
+    [-1.5, 2.7, 3.3, -0.8]
+]
+
+inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
+output = []
+
+for i in inputs:
+    if i > 0:
+        output.append(i)
+    elif(i <= 0):
+        output.append(0)
+
+print(output)
