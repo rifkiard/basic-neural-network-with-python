225736c2c26b40da68763a53ed49b88897f7d029
Rifki Ardiansyah <rifkiardiansyah670@gmail.com>
2023-02-22 16:21:03 +0700

    exponential function and normalization without numpy

--- p5.py
+++ p5.py
@@ -1,18 +1,42 @@
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
 
-np.random.seed(0)
+nnfs.init()
 
-# X is standard for input denotation.
+# X is standard for input denotation. y is target of classification.
 X = [
     [1, 2, 3, 2.5],
     [2.0, 5.0, -1.0, 2.0],
     [-1.5, 2.7, 3.3, -0.8]
 ]
 
+X, y = spiral_data(100, 3)
+
 inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
 output = []
 
-for i in inputs:
-    output.append(max(0, i))
 
-print(output)
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons) -> None:
+
+        # reserved, because we doesnt need to do transpose again.
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs) -> None:
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+
+layer1 = Layer_Dense(2, 5)
+activation1 = Activation_ReLU()
+
+layer1.forward(X)
+activation1.forward(layer1.output)
+
+print(activation1.output)
