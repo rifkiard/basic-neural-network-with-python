2a0713e3cae06229b44d4d81b5b0950158146c76
Rifki Ardiansyah <rifkiardiansyah670@gmail.com>
2023-02-22 16:32:38 +0700

    single input exponential and normalization

--- p6-1.py
+++ p6-1.py
@@ -1,22 +1,19 @@
 import math
+import numpy as np
+import nnfs
+
+nnfs.init()
 
 layer_outputs = [4.8, 1.21, 2.385]
 
 # euler's number
 E = math.e
 
-exp_values = []
-
-for output in layer_outputs:
-    exp_values.append(E**output)
+exp_values = np.exp(layer_outputs)
 
 print(exp_values)
 
-norm_base = sum(exp_values)
-norm_values = []
-
-for value in exp_values:
-    norm_values.append(value / norm_base)
+norm_values = exp_values / np.sum(exp_values)
 
 print(norm_values)
 print(sum(norm_values))
