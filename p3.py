3bb9fcff835b06db12b5642dc4013591ee8b3035
Rifki Ardiansyah <rifkiardiansyah670@gmail.com>
2023-02-22 12:13:11 +0700

    p4, two laywers

--- p3.py
+++ p3.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 inputs = [1, 2, 3, 2.5]
 
 weights = [
@@ -8,13 +10,5 @@ weights = [
 
 biases = [2, 3, 0.5]
 
-layer_outputs = []
-
-for neuron_weights, neuron_bias in zip(weights, biases):
-    neuron_output = 0
-    for n_input, weight in zip(inputs, neuron_weights):
-        neuron_output += n_input * weight
-    neuron_output += neuron_bias
-    layer_outputs.append(neuron_output)
-
-print(layer_outputs)
+output = np.dot(weights, inputs) + biases
+print(output)
