c593627d904cdc9a3b60ed4443c264d59cce964e
Rifki Ardiansyah <rifkiardiansyah670@gmail.com>
2023-02-22 15:35:55 +0700

    improved ReLU

--- p5.py
+++ p5.py
@@ -13,9 +13,6 @@ inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
 output = []
 
 for i in inputs:
-    if i > 0:
-        output.append(i)
-    elif(i <= 0):
-        output.append(0)
+    output.append(max(0, i))
 
 print(output)
